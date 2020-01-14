40679223519130a6d8edea8bae546b55f5589a3d
aravind <aravindsmenon94@gmail.com>
2020-01-14 10:46:22 +0530

    second commit

---
+++ LanguageFundementals/swapping.py
@@ -0,0 +1,15 @@
+no1=10
+no2=20
+print("Before swapping:",no1,",",no2)
+temp=no1
+no1=no2
+no2=temp
+print("After swapping:",no1,",",no2)
+
+num1=50
+num2=35
+print("Before swapping:",num1,",",num2)
+num1=num1+num2
+num2=num1-num2
+num1=num1-num2
+print("After swapping:",num1,",",num2)
